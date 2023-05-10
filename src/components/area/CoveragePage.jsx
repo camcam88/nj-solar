4f72e410a220482bca6d8183d2ef8f1322d00cd3
Cameron Duke <cameron_duke@apple.com>
2023-05-10 14:57:56 -0500

    changed coverage to area

---
+++ src/components/area/CoveragePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CoverageContent(){
+    return(
+        <div className='aboutSection items-center align-center pb-8'>
+            <h1>Coverage Area</h1>
+        </div>
+    )
+}
+
+export default CoverageContent;
--- src/pages/coverage.js
+++ src/pages/coverage.js
@@ -5,7 +5,7 @@ import Seo from "../components/seo"
 import PromoBanner from '../components/PromoBanner'
 import NavBar from '../components/nav/NavBar'
 import Footer from '../components/footer/Footer'
-import CoverageContent from '../components/coverage/CoveragePage'
+import CoverageContent from '../components/area/CoveragePage'
 
 function CoveragePage(){
 
